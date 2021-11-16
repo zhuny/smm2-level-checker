a207f8ba47765e26a1a819372a0805c3eeb6ffa4
zhuny <zhuny@kaist.ac.kr>
2021-11-17 00:51:34 +0900

    add modeling

--- server/model.py
+++ server/model.py
@@ -1,12 +1,49 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Model
+from sqlalchemy import Column, Integer
 
-db = SQLAlchemy()
+
+class ModelBase(Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
 
-class TestModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+db = SQLAlchemy(model_class=ModelBase)
+
+
+class Team(db.Model):
+    team_name = db.Column(db.Text)
+    discord_invite = db.Column(db.Text)
+    twitter_user = db.Column(db.Text)
+    primary_color = db.Column(db.Text)
+    secondary_color = db.Column(db.Text)
+    max_difficulty = db.Column(db.Integer)
+
+
+class Maker(db.Model):
+    code = db.Column(db.Text)
     name = db.Column(db.Text)
+
+
+class Level(db.Model):
+    code = db.Column(db.Text)
+    creator_id = db.Column(db.Integer, db.ForeignKey("maker.id"))
+    creator = db.relationship("Maker")
+    name = db.Column(db.Text)
+
+
+class LevelDifficulty(db.Model):
+    team_id = db.Column(db.Integer, db.ForeignKey("team.id"))
+    team = db.relationship("Team")
+    level_id = db.Column(db.Integer, db.ForeignKey("level.id"))
+    level = db.relationship("Level")
+
+    difficulty = db.Column(db.DECIMAL)
+    clears = db.Column(db.Integer)
+    likes = db.Column(db.Integer)
+    tags = db.Column(db.Text)
+
+    submission_date = db.Column(db.Date)
+    created_at = db.Column(db.Date)
 
 
 def init(app: Flask):
