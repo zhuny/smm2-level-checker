7e881dbf453c9bd71e073d59ff2ace8d6012c560
zhuny <zhuny@kaist.ac.kr>
2021-11-18 01:52:17 +0900

    client setting

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,35 @@
 <template lang="pug">
-div hi
+div {{ hello }}
 </template>
 
 <script>
+import gql from "graphql-tag";
+import request from "graphql-request";
+
 export default {
   name: "Home",
+  data() {
+    return {
+      hello: "",
+    };
+  },
+  created() {
+    request(
+      "http://localhost:5000/api/graphql",
+      gql`
+        {
+          allTeam {
+            edges {
+              node {
+                teamName
+              }
+            }
+          }
+        }
+      `
+    ).then((data) => {
+      console.log(data);
+    });
+  },
 };
 </script>
