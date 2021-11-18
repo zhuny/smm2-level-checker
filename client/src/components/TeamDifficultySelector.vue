85668939ce73d6e6f4f9ee67d0420c12fabc12af
zhuny <zhuny@kaist.ac.kr>
2021-11-18 23:39:25 +0900

    add page

---
+++ client/src/components/TeamDifficultySelector.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+div
+  div.p-2(:style="{backgroundColor: primaryColor}")
+    div.p-2
+      input.mr-2(type="checkbox")
+      label contain this team
+</template>
+
+<script>
+export default {
+  name: "TeamDifficultySelector",
+  props: ["teamName", "primaryColor", "secondaryColor", "maxDifficulty"],
+};
+</script>
+
+<style scoped></style>
---
+++ client/src/views/RandomPick.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+div
+  h3 Choose Condition
+  TeamDifficultySelector(
+    v-for="team in teamList"
+    :key="team.node.teamName"
+    :teamName="team.node.teamName"
+    :primaryColor="team.node.primaryColor"
+    :secondaryColor="team.node.secondaryColor"
+    :maxDifficulty="team.node.maxDifficulty"
+    v-model:selected="team.selected"
+    v-model:rangeStart="team.rangeStart"
+    v-model:rangeEnd="team.rangeEnd"
+    )
+</template>
+
+<script>
+import { client } from "@/util/graphql";
+import gql from "graphql-tag";
+import TeamDifficultySelector from "@/components/TeamDifficultySelector";
+
+export default {
+  name: "RandomPick",
+  components: {TeamDifficultySelector},
+  data() {
+    return {
+      teamList: [],
+    };
+  },
+  created() {
+    client
+      .query(
+        gql`
+          {
+            allTeam {
+              edges {
+                node {
+                  teamName
+                  primaryColor
+                  secondaryColor
+                  maxDifficulty
+                }
+              }
+            }
+          }
+        `
+      )
+      .then(({ allTeam: { edges } }) => {
+        this.teamList = edges.map(({ node }) => {
+          return {
+            node,
+            selected: false,
+            rangeStart: 0,
+            rangeEnd: 0,
+          };
+        });
+      });
+  },
+};
+</script>
+
+<style scoped></style>
