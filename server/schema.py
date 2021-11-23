00c3a48b9e7eea2e421aa1f44af76dcd701df551
zhuny <zhuny@kaist.ac.kr>
2021-11-23 19:54:56 +0900

    get random

--- server/schema.py
+++ server/schema.py
@@ -1,8 +1,12 @@
+import base64
+import random
+
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from sqlalchemy.orm import joinedload
 
-from server.model import Team, Maker, Level, LevelDifficulty
+from server.model import Team, Maker, Level, LevelDifficulty, db
 
 
 class TeamSchema(SQLAlchemyObjectType):
@@ -29,6 +33,31 @@ class LevelDifficultySchema(SQLAlchemyObjectType):
         interfaces = relay.Node,
 
 
+def _convert(team_id):
+    decoded_id = base64.b64decode(team_id).decode()
+    schema_name, row_id = decoded_id.split(':')
+    return int(row_id)
+
+
+class RandomLevelSchema(graphene.Mutation):
+    class Arguments:
+        team_id = relay.node.ID()
+        range_start = graphene.Decimal()
+        range_end = graphene.Decimal()
+
+    Output = LevelSchema
+
+    def mutate(self, info, team_id, range_start, range_end):
+        random_levels = LevelDifficultySchema.get_query(info).filter(
+            LevelDifficulty.team_id == _convert(team_id),
+            LevelDifficulty.difficulty >= range_start,
+            LevelDifficulty.difficulty <= range_end
+        ).options(
+            joinedload(LevelDifficulty.level)
+        ).limit(10).all()  # 적당히 랜덤하게 쿼리 작성할 수 있도록 수정
+        return random.choice(random_levels).level
+
+
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
 
@@ -37,4 +66,8 @@ class Query(graphene.ObjectType):
     all_level = SQLAlchemyConnectionField(LevelSchema.connection)
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    random_level = RandomLevelSchema.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
