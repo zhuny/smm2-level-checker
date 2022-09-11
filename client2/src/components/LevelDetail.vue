c27a9dd79c8bc33942de258a54ab7fe87d2dec1e
zhuny <zhuny@kaist.ac.kr>
2022-09-11 18:38:51 +0900

    load detail request

--- client2/src/components/LevelDetail.vue
+++ client2/src/components/LevelDetail.vue
@@ -3,12 +3,51 @@ v-container
 </template>
 
 <script>
+import { client } from "../plugins/graphql";
+import { gql } from "graphql-request";
+
 export default {
   name: "LevelDetail",
   data() {
     return {
       levelId: this.$route.params.levelId,
     };
+  },
+  created() {
+    client
+      .request(
+        gql`
+          query getLevel($levelId: ID!) {
+            level(id: $levelId) {
+              code
+              name
+              creator {
+                name
+              }
+              clearInfo {
+                clearAt
+              }
+              difficultyList {
+                edges {
+                  node {
+                    id
+                    difficulty
+                    team {
+                      teamName
+                      primaryColor
+                      secondaryColor
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+        { levelId: this.levelId }
+      )
+      .then((info) => {
+        console.log(info);
+      });
   },
 };
 </script>
