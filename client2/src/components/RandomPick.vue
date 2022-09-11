ffd832f28ebdb3e49e6a3fe1c63afaf832076edf
zhuny <zhuny@kaist.ac.kr>
2022-09-11 11:33:46 +0900

    add random pick section

--- client2/src/components/RandomPick.vue
+++ client2/src/components/RandomPick.vue
@@ -1,18 +1,20 @@
 <template lang="pug">
 v-container
-  v-card.mx-auto
-    v-list(
-      :items="teamList"
-      item-title="teamName"
-    )
+  RandomTeamPick.mt-4(
+    :team-name="team.teamName"
+    v-for="team in teamList"
+    key="team.id"
+  )
 </template>
 
 <script>
 import { client } from "../plugins/graphql";
 import { gql } from "graphql-request";
+import RandomTeamPick from "./RandomTeamPick.vue";
 
 export default {
   name: "RandomPick",
+  components: { RandomTeamPick },
   data() {
     return {
       teamList: [],
---
+++ client2/src/components/RandomTeamPick.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+v-card
+  v-card-title {{ teamName }}
+</template>
+
+<script>
+export default {
+  name: "RandomTeamPick",
+  data() {
+    return {};
+  },
+  props: {
+    teamName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
